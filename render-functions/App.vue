100b482a408095f0e87a1c9a58078f03214eff7f
Andres Ruiz <aruiz@grupooctano.com.mx>
2019-11-11 11:47:36 -0600

    Creación de un componente con createElement

--- render-functions/App.vue
+++ render-functions/App.vue
@@ -1,12 +1,24 @@
 <template>
     <div id="app">
-        <button> Toggle Modal </button>
+        <button @click.prevent="toggleHandler"> Toggle Modal </button>
+        <modal-create-element :show="show" message="Hello world"> </modal-create-element>
     </div>
 </template>
 
 <script>
-
+import ModalCreateElement from './ModalCreateElement.vue'
 export default {
-    name: 'App'
+    name: 'App',
+    data() {
+        return {
+            show: false,
+        }
+    },
+    components: { ModalCreateElement },
+    methods: {
+        toggleHandler() {
+          this.show = !this.show  
+        }
+    },
 };
 </script>
---
+++ render-functions/ModalCreateElement.vue
@@ -0,0 +1,45 @@
+<script>
+
+export default {
+    name: 'ModalCreateElement',
+    props: {
+        show : {
+            type: Boolean,
+            default: true
+        },
+        message : {
+            type: String,
+            required: true
+        }
+    },
+    render(createElement) {
+        return (this.show) ? createElement('div', {
+            class: 'modal'
+        }, [
+            createElement('p', {
+                class: 'message'
+            }, this.message ),
+            createElement('button', {
+                class: 'btn',
+                on: {
+                   click: this.clickOkHandler, 
+                }
+            }, 'Ok'),
+            createElement('button', {
+                class: 'btn',
+                on: {
+                   click: this.clickCancelHandler, 
+                }
+            }, 'Cancel'),
+        ]) : null
+    },
+    methods: {
+        clickCancelHandler () {
+            console.log('Cli en cancelar');
+        },
+        clickOkHandler () {
+            console.log('Cli en OK');
+        }
+    } 
+};
+</script>
