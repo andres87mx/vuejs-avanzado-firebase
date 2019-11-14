2103c9fbf2b5398893c3e24cfe5a1c77c73378a0
Andres Ruiz <aruiz@grupooctano.com.mx>
2019-11-14 16:15:27 -0600

    Obtener datos usando Vuex

--- platzi-rooms/src/views/user/ProfilePage.vue
+++ platzi-rooms/src/views/user/ProfilePage.vue
@@ -26,13 +26,15 @@
               <div class="mb-4">
                 <label class="input__label" for="name">Name</label>
                 <div class="form__field relative">
-                  <input class="input__field" id="name" type="text" placeholder="Bruce Wayne">
+                  <input v-model="profile.name"
+                  class="input__field" id="name" type="text" placeholder="Bruce Wayne">
                 </div>
               </div>
               <div class="mb-4">
                 <label class="input__label" for="username">Username</label>
                 <div class="form__field relative">
-                  <input class="input__field" id="username" type="text" placeholder="bruce.wayne">
+                  <input v-model="profile.username"
+                  class="input__field" id="username" type="text" placeholder="bruce.wayne">
                 </div>
               </div>
               <div class="mb-4">
@@ -40,7 +42,7 @@
                   class="input__label"
                   for="avatar">Avatar</label>
                 <div class="form__field relative">
-                  <input class="input__field" id="avatar" type="text" placeholder="https://avatars1.githubusercontent.com/u/9919?s=500&v=4">
+                  <input v-model="profile.avatar" class="input__field" id="avatar" type="text" placeholder="https://avatars1.githubusercontent.com/u/9919?s=500&v=4">
                 </div>
               </div>
               <div class="mb-4">
@@ -49,6 +51,7 @@
                   for="email">Email</label>
                 <div class="form__field relative">
                   <input
+                    v-model="profile.email"
                     class="input__field"
                     id="email"
                     type="text"
@@ -61,6 +64,7 @@
                   for="email">Bio</label>
                 <div class="form__field relative">
                   <textarea
+                    v-model="profile.bio"
                     class="input__field"
                     rows="5"
                     id="email"
@@ -87,10 +91,30 @@
 </template>
 
 <script>
+import { mapGetters } from 'vuex';
 import PageLayout from '@/layouts/PageLayout.vue';
 
 export default {
   name: 'ProfilePage',
+  data() {
+    return {
+      profile: {
+        name: '',
+        username: '',
+        avatar: '',
+        email: '',
+        bio: '',
+      },
+    };
+  },
+  mounted() {
+    this.profile = this.user;
+  },
+  computed: {
+    ...mapGetters({
+      user: 'authUser',
+    }),
+  },
   components: {
     PageLayout,
   },
