bc8ddb74289eab90c24d9af6359d04597fee221b
Andres Ruiz <aruiz@grupooctano.com.mx>
2019-11-16 09:44:49 -0600

    Hide credencials on firebase

---
+++ platzi-rooms/src/config.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyAHh6JXxxfjVrE3HdbGLtEQ0SMf1Ltqubc',
+  authDomain: 'platzi-rooms-73f8a.firebaseapp.com',
+  databaseURL: 'https://platzi-rooms-73f8a.firebaseio.com',
+  projectId: 'platzi-rooms-73f8a',
+  storageBucket: 'platzi-rooms-73f8a.appspot.com',
+  messagingSenderId: '45930960829',
+  appId: '1:45930960829:web:3f93cec1664604d9a864d4',
+  measurementId: 'G-2Z60DN8YR0',
+};
+export default firebaseConfig;
